5c457a3a4790be92dfcb8899023ab5c1b5645a31
Antti Kumavaara <antti.kumavaara@tuni.fi>
2020-04-06 22:57:48 +0300

    Added script that starts the usual apps I use dayly

---
+++ start_apps.py
@@ -0,0 +1,6 @@
+import subprocess
+import os
+subprocess.call('D:\\Muut\\Discord\\Update.exe --processStart Discord.exe')
+subprocess.call('C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe')
+os.startfile('D:\\tg\\Telegram Desktop\\Telegram.exe')
+exit
